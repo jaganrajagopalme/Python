f67e0e6c5102350929ae9950887976dffa5a6643
unknown <jaganrajagopalme@gmail.com>
2020-03-16 18:38:00 +0000

    adding files

---
+++ LambdaS3Upload.py
@@ -0,0 +1,32 @@
+import boto3
+import json
+from botocore.exceptions
+def lambda_handler(event,context):
+    # Reading the files
+    s3= boto3.client('s3')
+    with open("FILE_NAME", "rb") as f:
+    s3.upload_fileobj(f, "BUCKET_NAME", "OBJECT_NAME")
+    
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
